81bc6ee36a7c152a70b706689405c8f77669fe40
sasparavinit@gmail.com <sasparavinit@gmail.com>
2025-12-25 18:58:12 +0530

    fix: Update axios instance to use VITE_API_URL for base URL configuration

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from "axios"
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE == "development" ? "https://chatify-server-1-t3ob.onrender.com/api/v1" : "",
-    withCredentials: true,
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
